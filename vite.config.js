e1119cb57e08220e1c1542665aef79bff74cd077
Pablo Fabbian <pablo.fabbian@gmail.com>
2024-10-09 23:55:00 -0300

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://pablofabbian.dev.com',
+  base: 'https://pablofabbian.vercel.app',
   server: {
     host: true,
   },
